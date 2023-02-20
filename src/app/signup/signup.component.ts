14f46380feca72c5286028591767c47cb1d034f3
kfahad <fahadkhan@Fahads-Mac-mini.local>
2023-02-20 17:48:48 +0600

    signup component modified

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,10 +1,12 @@
+import { error } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { NgxUiLoaderService } from 'ngx-ui-loader';
 import { SnackbarService } from '../services/snackbar.service';
 import { UserService } from '../services/user.service';
+import { GlobalConstants } from '../shared/global-constants';
 
 @Component({
   selector: 'app-signup',
@@ -15,17 +17,54 @@ export class SignupComponent implements OnInit {
 
   password = true;
   confirmPassword = true;
-  signupForm:any = FormGroup;
-  responseMessage : any;
+  signupForm: any = FormGroup;
+  responseMessage: any;
 
-  constructor(private formBuilder : FormBuilder,
-    private router : Router,
+  constructor(private formBuilder: FormBuilder,
+    private router: Router,
     private userService: UserService,
-    private snackbarService : SnackbarService,
-    public dialogRef : MatDialogRef<SignupComponent>,
-    private ngxService : NgxUiLoaderService) { }
+    private snackbarService: SnackbarService,
+    public dialogRef: MatDialogRef<SignupComponent>,
+    private ngxService: NgxUiLoaderService) { }
 
   ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      name: [null, [Validators.required, Validators.pattern(GlobalConstants.nameRegex)]],
+      email: [null, [Validators.required, Validators.pattern(GlobalConstants.emailRegex)]],
+      contactNumber: [null, [Validators.required, Validators.pattern(GlobalConstants.contactNumberRegex)]],
+      password: [null, [Validators.required]],
+      confirmPassword: [null, [Validators.required]],
+
+    })
   }
 
+  validateSubmit() {
+    if (this.signupForm.controls['password'].value != this.signupForm.controls['confirmPassword'].value)
+      return true;
+    else
+      return false;
+  }
+  
+  // handleSubmit(){
+  //   this.ngxService.start();
+  //   var formData = this.signupForm.value;
+  //   var data = {
+  //     name : formData.name,
+  //     email : formData.email,
+  //     contactNumber : formData.contactNumber,
+  //     password : formData.password
+  //   }
+
+  //   this.userService.signup(data).subscribe((res : any){
+  //      this.ngxService.stop();
+  //      this.dialogRef.close();
+  //      this.responseMessage = res?.message;
+  //      this.snackbarService.openSncakBar(this.responseMessage, "");
+  //      this.router.navigate(['/'])
+  //   }
+
+  //   }
+    
+  
+   
 }
